6176bb666f5103a0ae777534edb20f7510f664ae
Sugar025016 <47650172+Sugar025016@users.noreply.github.com>
2020-05-15 23:25:40 +0800

    test2

--- src/main/java/com/example/booking/BookingApplication.java
+++ src/main/java/com/example/booking/BookingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.booking.*")
 @EnableJpaRepositories(basePackages = "com.example.booking.repository")

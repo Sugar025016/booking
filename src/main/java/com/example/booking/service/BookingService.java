94278f6b83952e0cb8a1b38f35ecd6606f22a822
Sugar025016 <47650172+Sugar025016@users.noreply.github.com>
2020-05-16 21:00:19 +0800

    booking 修改 application.properties 的 spring.datasource.url

--- src/main/java/com/example/booking/service/BookingService.java
+++ src/main/java/com/example/booking/service/BookingService.java
@@ -20,31 +20,4 @@ public interface BookingService {
 
 
 
-//    UserDevResponseV2_1 getUserV2_1(int id);
-//
-//    UserListResponseV2_1 getUserListV2_1(String keyword, List<Integer> teamIdList, int pageNumber, int pageSize);
-//
-//    void updateV2_1(UserUpdateRequestV2_1 request);
-//
-//    @Deprecated
-//    Page<UserListResponse> getUserList(SearchUserRequest model);
-//
-//    @Deprecated
-//    UserDevResponse getUser(int id);
-//
-//    @Deprecated
-//    void update(UserUpdateRequest request);
-//
-//    void addUserByLdap(List<String> accounts);
-//
-//    List<String> getPermission();
-//
-////    Boolean insertGroupPay(UserPayRequest model) throws Exception;
-////
-////    Boolean insertGroupPayTotal(UserPayTotalRequest model) throws Exception;
-//
-//
-//    List<RoleResponse> getRole();
-//
-//    List<UserAllListResponse> getAllUserList();
 }
